a1fe58814eef7d543c6ca92d22863927be328cc3
Kennedy <ibehkennedyj@gmail.com>
2020-07-19 23:24:52 +0300

    feat: add Deezer track model

---
+++ src/deezer/model/Track.java
@@ -0,0 +1,26 @@
+package deezer.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.json.JsonObject;
+
+@Data
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+public class Track {
+
+    private final String title;
+    private final String artist;
+    private final String album;
+
+    public static Track from(JsonObject json) {
+        return Track.builder()
+                .title(json.getString("title"))
+                .artist(json.getJsonObject("artist").getString("name"))
+                .album(json.getJsonObject("album").getString("title"))
+                .build();
+    }
+}
